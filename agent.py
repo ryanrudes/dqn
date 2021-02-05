eb1381dcd00c6df8c54e694f076b8c977f32de0e
Ryan Rudes <ryanrudes@gmail.com>
2021-02-04 21:02:18 -0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 from tensorflow.keras.losses import *
 from tensorflow.keras.optimizers import *
+from replay_memory import ReplayMemory
 
 memory = ReplayMemory(max_replay_memory)
 model = make(num_actions)
