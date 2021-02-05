e09895f5dfdeb476b93e4f43fec3a0db64e335e3
Ryan Rudes <ryanrudes@gmail.com>
2021-02-04 21:00:55 -0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,1 +1,7 @@
+memory = ReplayMemory(max_replay_memory)
+model = make(num_actions)
+target = make(num_actions)
+target.set_weights(model.get_weights())
 
+optimizer = Adam(learning_rate, clipnorm = clipnorm)
+lossfn = Huber()
