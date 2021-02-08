567f399014707f9fa6e23ac954d6c7e28ce31cfe
Ryan Rudes <ryanrudes@gmail.com>
2021-02-08 18:04:45 -0500

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,7 +1,8 @@
 import gym
-from config import env_id
+from config import env_id, seed
 from wrapper import FrameStack
 
 env = gym.make(env_id)
+env.seed(seed)
 env = FrameStack(env)
 num_actions = env.action_space.n
