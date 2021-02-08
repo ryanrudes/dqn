6d55289a16eca3d7b2c6f5f94215635bc0e93618
Ryan Rudes <ryanrudes@gmail.com>
2021-02-08 18:07:56 -0500

    Update config.py

--- config.py
+++ config.py
@@ -18,4 +18,4 @@ learning_rate = float(config["LEARNING_RATE"])
 clipnorm = float(config["CLIPNORM"])
 seed = int(config["SEED"])
 render = int(config["RENDER"])
-optimizerfn = config["OPTIMIZER"]
+lossfn = config["LOSS"]
